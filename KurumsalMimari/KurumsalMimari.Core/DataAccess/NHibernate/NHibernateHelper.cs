35256271028d18d78a8a7989f740aedb07a9cf20
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-15 23:16:41 +0300

    NHibernateHelper implementation

---
+++ KurumsalMimari/KurumsalMimari.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NHibernate;
+
+namespace KurumsalMimari.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public virtual ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    _sessionFactory = InitializeFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
