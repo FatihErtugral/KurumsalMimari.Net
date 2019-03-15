7a4f921f9abbe30a243de657add72a33c9110f0f
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-15 22:29:02 +0300

    IEntityRepository

---
+++ KurumsalMimari/KurumsalMimari.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using KurumsalMimari.Core.Entities;
+
+namespace KurumsalMimari.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T: class, IEntity, new()
+    {
+        List<T> GetList(Expression<Func<T,bool>> filter = null);
+        void Delete(T entity);
+        T Get(Expression<Func<T, bool>> filter);
+        T Add(T entity);
+        T Update(T entity);
+    }
+}
--- KurumsalMimari/KurumsalMimari.Core/Entities/IEntity.cs
+++ KurumsalMimari/KurumsalMimari.Core/Entities/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KurumsalMimari.Core
+namespace KurumsalMimari.Core.Entities
 {
-    public class Class1
+    public interface IEntity
     {
     }
 }
