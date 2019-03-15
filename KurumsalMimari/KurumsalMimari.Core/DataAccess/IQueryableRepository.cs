8214ba118a18e63aa7be363398f302822fe85c9c
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-15 22:32:27 +0300

    IQueryableRepository

---
+++ KurumsalMimari/KurumsalMimari.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using KurumsalMimari.Core.Entities;
+
+namespace KurumsalMimari.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T:class, IEntity, new()
+    {
+        IQueryable<T> Table { get;  }
+    }
+}
