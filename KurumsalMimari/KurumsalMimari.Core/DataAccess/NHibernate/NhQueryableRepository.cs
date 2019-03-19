94a241f716e7492200df43063a57f3a3cb6b01c2
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-19 17:29:31 +0300

    NhQueryableRepository implementation

---
+++ KurumsalMimari/KurumsalMimari.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Core.Entities;
+
+namespace KurumsalMimari.Core.DataAccess.NHibernate
+{
+    class NhQueryableRepository<TEntity> : IQueryableRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        IQueryable<TEntity> _entities;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+        public IQueryable<TEntity> Table => this.Entities;
+
+        public virtual IQueryable<TEntity> Entities
+        {
+            get
+            {
+                if(_entities == null)
+                {
+                    _entities = _nHibernateHelper.OpenSession().Query<TEntity>();
+                }
+                return _entities;
+            }
+        }
+    }
+}
