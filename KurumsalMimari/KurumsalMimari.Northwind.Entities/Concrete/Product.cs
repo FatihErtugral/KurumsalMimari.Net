83e5abaa718ed177c6cc0ef7b8fccc158ef0d452
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-19 17:39:42 +0300

    Veri Erişim Katmanında IEntityRepository Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Core.Entities;
+
+namespace KurumsalMimari.Northwind.Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
