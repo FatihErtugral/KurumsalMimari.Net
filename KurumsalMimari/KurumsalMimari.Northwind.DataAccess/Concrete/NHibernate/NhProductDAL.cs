0a104f6dc1094e181c282bfbf4989515c21baa49
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 23:09:56 +0300

    Nhibernate Complex Tiplerle Çalışmak

--- KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/NhProductDAL.cs
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/NhProductDAL.cs
@@ -12,13 +12,27 @@ namespace KurumsalMimari.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDAL : NhEntityRepositoryBase<Product>, IProductDAL
     {
+        private NHibernateHelper _nHibernateHelper;
         public NhProductDAL(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
+            _nHibernateHelper = nHibernateHelper;
         }
 
         public List<ProductDetail> GetProductDetails()
         {
-            throw new NotImplementedException();
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>()
+                             on p.CategoryID equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductID,
+                                 CategoryName = c.CategoryName,
+                                 ProductName = p.ProductName
+                             };
+                return result.ToList();
+            }
         }
     }
 }
