88043d3a07a824e2a0f6df57522c0a31869246c2
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 10:14:55 +0300

    NHibernate ProductDAL Implementeasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/NhProductDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Core.DataAccess.NHibernate;
+using KurumsalMimari.Northwind.DataAccess.Abstract;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhProductDAL : NhEntityRepositoryBase<Product>, IProductDAL
+    {
+        public NhProductDAL(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
