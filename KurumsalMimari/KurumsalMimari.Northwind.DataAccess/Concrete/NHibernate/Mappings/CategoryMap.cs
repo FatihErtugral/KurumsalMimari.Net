c8e81f119e5ac266d5c4e77f57e8f1f07a250457
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 11:26:05 +0300

    NHibernate için Framework Kullanımı Toplu Tekrarı (category için)

---
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/Mappings/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class CategoryMap:ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Table(@"Categories");
+            LazyLoad();
+            Id(x => x.CategoryId).Column("CategoryId");
+            Map(x => x.CategoryName).Column("CategoryName");
+        }
+    }
+}
