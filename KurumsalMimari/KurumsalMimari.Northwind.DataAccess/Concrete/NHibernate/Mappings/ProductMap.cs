411f49b3f14c9f2522f08bfc4a6162fbc565aca0
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 10:08:42 +0300

    NHibernate için Mapping Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class ProductMap:ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table(@"Products");
+            LazyLoad();
+            Id(x => x.ProductID).Column("ProductID");
+            Map(x => x.CategoryID).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+        }
+    }
+}
