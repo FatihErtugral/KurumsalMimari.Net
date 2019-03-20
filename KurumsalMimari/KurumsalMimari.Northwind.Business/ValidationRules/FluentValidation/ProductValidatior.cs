fe0c735f5d4873d37ee96c1479965d2d30797d92
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 23:28:07 +0300

    Fluent Validation Yapılandırması

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/ValidationRules/FluentValidation/ProductValidatior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidatior: AbstractValidator<Product>
+    {
+        public ProductValidatior()
+        {
+            RuleFor(p => p.CategoryID).NotEmpty().WithMessage("Kendi Mesajını koyabilirsin");
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.ProductName).Length(2, 20);
+            RuleFor(p => p.UnitPrice).GreaterThan(20).When(p =>p.CategoryID == 1);
+            //RuleFor(p => p.ProductName).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
