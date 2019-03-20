2e8911c9e3368d1eb8b8adb38a63152762b357b0
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 09:51:13 +0300

    Entity Framework için birim testlerinin yazılması 2

--- KurumsalMimari/KurumsalMimari.Northwind.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
@@ -15,5 +15,13 @@ namespace KurumsalMimari.Northwind.DataAccess.Tests.EntityFrameworkTests
 
             Assert.AreEqual(78, result.Count);
         }
+        [TestMethod]
+        public void Get_all_with_parameter_returns_fşktered_products()
+        {
+            EfProductDAL productDAL = new EfProductDAL();
+            var result = productDAL.GetList(p=> p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(4, result.Count);
+        }
     }
 }
