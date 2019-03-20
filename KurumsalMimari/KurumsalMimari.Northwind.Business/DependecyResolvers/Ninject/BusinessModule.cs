34854f69871009245c415533cbc4b8816b9eb367
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-21 02:15:08 +0300

    Business Module Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/DependecyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Northwind.Business.Abstract;
+using KurumsalMimari.Northwind.Business.Concrete.Managers;
+using KurumsalMimari.Northwind.DataAccess.Abstract;
+using KurumsalMimari.Northwind.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+
+namespace KurumsalMimari.Northwind.Business.DependecyResolvers.Ninject
+{
+    public class BusinessModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDAL>().To<EfProductDAL>();
+        }
+    }
+}
