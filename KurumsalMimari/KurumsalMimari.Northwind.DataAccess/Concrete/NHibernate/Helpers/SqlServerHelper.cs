b47911bd5c05ea2ffba0127c2560141b8d6ebd32
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-20 10:04:24 +0300

    NHibernate Sql Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using KurumsalMimari.Core.DataAccess.NHibernate;
+using NHibernate;
+
+namespace KurumsalMimari.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    class SqlServerHelper : NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012
+                .ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext")))
+                .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .BuildSessionFactory();
+                
+        }
+    }
+}
