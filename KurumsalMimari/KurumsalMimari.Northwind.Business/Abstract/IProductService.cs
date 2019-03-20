be277187e05f20845e72cf5d37e06be3f42e6e08
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-21 00:02:11 +0300

    İş Katmanının Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KurumsalMimari.Northwind.Business.Abstract;
+using KurumsalMimari.Northwind.DataAccess.Abstract;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDAL _productDAL;
+
+        public ProductManager(IProductDAL productDAL)
+        {
+            _productDAL = productDAL;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDAL.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDAL.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDAL.Get(p => p.ProductID == id);
+        }
+    }
+}
