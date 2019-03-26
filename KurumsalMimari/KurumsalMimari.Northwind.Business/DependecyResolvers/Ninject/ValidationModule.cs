63fd440fd812d828222fd791f865f0fd5619246c
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-26 10:26:39 +0300

    Client Side Validation İçin Dependency Injection Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/DependecyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using KurumsalMimari.Northwind.Business.ValidationRules.FluentValidation;
+using KurumsalMimari.Northwind.Entities.Concrete;
+using Ninject.Modules;
+
+namespace KurumsalMimari.Northwind.Business.DependecyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Product>>().To<ProductValidatior>().InSingletonScope();
+        }
+    }
+}
