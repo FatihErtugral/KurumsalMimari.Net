ef5182e6e60aa715c90010ac94b584922898745d
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-27 13:10:22 +0300

    MVC Framework Entegrasyonu İçin İlk Adımlar

--- KurumsalMimari/KurumsalMimari.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
+++ KurumsalMimari/KurumsalMimari.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
@@ -7,6 +7,7 @@ using log4net;
 
 namespace KurumsalMimari.Core.CrossCuttingConcerns.Logging.Log4Net
 {
+    [Serializable]
     public class LoggerService
     {
         private ILog _log;
---
+++ KurumsalMimari/KurumsalMimari.Northwind.MvcWebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.MvcWebUI.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
