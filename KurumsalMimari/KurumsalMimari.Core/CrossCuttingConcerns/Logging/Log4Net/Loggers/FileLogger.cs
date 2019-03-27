a15ede5f1fe734d33a319637b08aee9ad64dd8cd
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-27 11:45:49 +0300

    FileLogger Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace KurumsalMimari.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class FileLogger:LoggerService
+    {
+        public FileLogger() : base(LogManager.GetLogger("JsonFileLogger"))
+        {
+
+        }
+    }
+}
