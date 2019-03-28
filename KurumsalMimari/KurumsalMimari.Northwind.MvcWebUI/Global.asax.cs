87368d8d033f95c620229ca67aab6c84dfeda5a3
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-28 14:52:11 +0300

    ASP.NET MVC Implementasyonu - PostAuthenticateRequest Operasyonu ile Identity Implementasyonu

--- KurumsalMimari/KurumsalMimari.Northwind.MvcWebUI/Global.asax.cs
+++ KurumsalMimari/KurumsalMimari.Northwind.MvcWebUI/Global.asax.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
+using KurumsalMimari.Core.CrossCuttingConcerns.Security.Web;
 using KurumsalMimari.Core.Utilities.Mvc.Infrastructure;
 using KurumsalMimari.Northwind.Business.DependecyResolvers.Ninject;
 
@@ -17,6 +21,44 @@ namespace KurumsalMimari.Northwind.MvcWebUI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new BusinessModule()));
+        }
+
+        public override void Init()
+        {
+            PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init();
+        }
+
+        private void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+
+                if (authCookie != null)
+                {
+                    return;
+                }
+
+                var encTicket = authCookie.Value;
+                if (String.IsNullOrEmpty(encTicket))
+                {
+                    return;
+                }
+
+                var ticket = FormsAuthentication.Decrypt(encTicket);
+
+                var securityUtilities = new SecurityUtilities();
+                var identity = securityUtilities.FormsAuthTicketToIdentity(ticket);
+                var principal = new GenericPrincipal(identity, identity.Roles);
+
+                HttpContext.Current.User = principal;
+                Thread.CurrentPrincipal = principal;
+            }
+            catch 
+            {
+               
+            }
         }
     }
 }
