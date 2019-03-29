9c9734e307000261c6c0ab8d8e13be1b67e8b994
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-29 23:27:58 +0300

    Web API'ye Rol Bazlı Altyapının Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/DependecyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace KurumsalMimari.Northwind.Business.DependecyResolvers.Ninject
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.Get<T>();
+        }
+    }
+}
--- KurumsalMimari/KurumsalMimari.Northwind.WebApi/MessageHandlers/AutohenticationHandler.cs
+++ KurumsalMimari/KurumsalMimari.Northwind.WebApi/MessageHandlers/AutohenticationHandler.cs
@@ -7,6 +7,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using KurumsalMimari.Northwind.Business.Abstract;
+using KurumsalMimari.Northwind.Business.DependecyResolvers.Ninject;
+using KurumsalMimari.Northwind.Entities.Concrete;
+using System.Linq;
 
 namespace KurumsalMimari.Northwind.WebApi.MessageHandlers
 {
@@ -23,9 +27,14 @@ namespace KurumsalMimari.Northwind.WebApi.MessageHandlers
                     string decodedString = Encoding.UTF8.GetString(data);
                     string[] tokenValues = decodedString.Split(':');
 
-                    if (tokenValues[0] == "fatih" && tokenValues[1] == "12345")
+                    IUserService userService = InstanceFactory.GetInstance<IUserService>();
+
+                    User user = userService.GetByUserNameAndPassword(tokenValues[0], tokenValues[1]);
+                    if (user != null)
                     {
-                        IPrincipal principal = new GenericPrincipal(new GenericIdentity(tokenValues[0]), new[] { "Admin" });
+                        IPrincipal principal = new GenericPrincipal(
+                            new GenericIdentity(tokenValues[0]), 
+                            userService.GetUserRoles(user).Select(u => u.RoleName).ToArray());
                         Thread.CurrentPrincipal = principal;
                         HttpContext.Current.User = principal;
                     }
