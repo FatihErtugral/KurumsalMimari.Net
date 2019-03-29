da4f9fa9717e1370aa9d197f36cee5afc0f1a221
FatihErtugral <fatih.ertugral@outlook.com>
2019-03-29 22:12:05 +0300

    AutoMapper Profile Implementasyonu

---
+++ KurumsalMimari/KurumsalMimari.Northwind.Business/Mappings/AutoMapper/Profiles/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using KurumsalMimari.Northwind.Entities.Concrete;
+
+namespace KurumsalMimari.Northwind.Business.Mappings.AutoMapper.Profiles
+{
+    public class BusinessProfile : Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<Product, Product>();
+        }
+    }
+}
